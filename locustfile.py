dae85afa65cce96fe4275772af08108556c5d644
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2026-02-21 23:02:59 -0700

    completed section for person 1

---
+++ locustfile.py
@@ -0,0 +1,30 @@
+from locust import HttpUser, task, between
+import random
+
+class TeaStoreUser(HttpUser):
+    wait_time = between(1, 3)
+
+    # -------------------
+    # Browsing Workload
+    # -------------------
+    @task(3)
+    def browse_home(self):
+        self.client.get("/")
+
+    @task(3)
+    def browse_category(self):
+        category_id = random.randint(1, 5)
+        self.client.get(f"/category?id={category_id}")
+
+    @task(2)
+    def view_product(self):
+        product_id = random.randint(1, 50)
+        self.client.get(f"/product?id={product_id}")
+
+    @task(1)
+    def login(self):
+        self.client.post(
+            "/loginAction",
+            data={"username": "user", "password": "password"},
+            catch_response=True
+        )
